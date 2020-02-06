911f85346a363011989ede1709749d7a8fedf221
Guy Avraham <guy1812@gmail.com>
2020-02-06 08:41:52 +0200

    i18n

--- pages/Main.js
+++ pages/Main.js
@@ -3,20 +3,26 @@ import { StyleSheet, View, Text } from "react-native";
 import MaterialButtonViolet from "../components/MaterialButtonViolet";
 import Anchor from "../components/anchor";
 import BrowserModal from "../components/browserModal";
+import { translated } from "../utils/localization";
 
 function Main(props) {
   console.log("render");
   return (
     <View style={styles.container}>
-      <Text style={styles.loremIpsum}>{props.name}, Welcome back</Text>
-      <Anchor text="Chat" style={styles.chat} href="http://m.me/kbpuretest" />
+      <Text style={styles.loremIpsum}>{props.name}, {translated("Welcome")}</Text>
+      <Anchor text={translated("Chat")} style={styles.chat} href="http://m.me/kbpuretest" />
       <BrowserModal
-        text="Shop"
+        text={translated("Shop")}
         style={styles.shop}
         href="https://gsjewelryhouse.com/"
       />
+      <BrowserModal
+        text={translated("Content")}
+        style={styles.shop}
+        href="http://15.188.181.32/"
+      />
       <MaterialButtonViolet
-        text1="Logout"
+        text1={translated("Logout")}
         style={styles.logout}
         onPress={props.logout}
       ></MaterialButtonViolet>
