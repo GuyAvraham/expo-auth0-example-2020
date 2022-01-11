37635e81a0c41bf32be422b6676635acd0becb44
Ian Lyles <ian@icldesign.com>
2022-01-11 15:04:21 -0800

    my attmpt

--- Config/env.ts
+++ Config/env.ts
@@ -1,27 +1,28 @@
 const ENV = {
   dev: {
-    apiUrl: "localhost",
+    apiUrl: 'localhost',
     auth0: {
-      auth0Domain: 'dev-XXX.auth0.com',
+      auth0Domain: 'dev-bz-00bvk.us.auth0.com',
       passwordlessClient: {
-        client_id: 'REPLACE',
-        clientId: 'REPLACE',
+        client_id: 'Ytb0BOT1vRuVBWRpl4KRJaCVQHLYvne9',
+        clientId: 'Ytb0BOT1vRuVBWRpl4KRJaCVQHLYvne9',
         allowedConnections: ['sms'],
-        scope: 'openid profile read:current_user update:current_user_identities',
+        scope:
+          'openid profile read:current_user update:current_user_identities',
         // response_type:
         // id_token will return a JWT token with the profile as described on the scope
         // token will return access_token to use with further api calls
         response_type: 'token id_token',
-        nonce: 'nonce' // ideally, this will be a random value
+        nonce: 'nonce', // ideally, this will be a random value
       },
-    }
+    },
   },
   staging: {
-    apiUrl: "[your.staging.api.here]"
+    apiUrl: '[your.staging.api.here]',
   },
   prod: {
-    apiUrl: "[your.production.api.here]"
-  }
+    apiUrl: '[your.production.api.here]',
+  },
 };
 
 export default ENV;
